8f5b1fb1bce30585e110a3ac0f2f32915d8634fd
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2014-04-06 21:02:32 +0200

    Added support for analog inputs with limited range

--- Arduino/libraries/AI/AI.cpp
+++ Arduino/libraries/AI/AI.cpp
@@ -1,11 +1,14 @@
 #include "Arduino.h"
 #include "AI.h"
 
-AI::AI(int pin, float rangeLow, float rangeHigh, float lolo, float lo, float hi, float hihi, bool enableBTA) {
+AI::AI(int pin, float rangeLow, float rangeHigh, float lolo, float lo, float hi, float hihi, bool enableBTA, int rawLow, int rawHigh) {
 	_pin = pin;
 
 	_rangeLow = rangeLow;
 	_rangeHigh = rangeHigh;
+
+	_rawLow = rawLow;
+	_rawHigh = rawHigh;
 
 	_lolo = lolo;
 	_lo = lo;
@@ -30,7 +33,7 @@ AI::AI(int pin, float rangeLow, float rangeHigh, float lolo, float lo, float hi,
 
 void AI::loop(bool t100ms, bool b1s) {
 	_raw = analogRead(_pin);
-	_value = ((float)_raw) * ((_rangeHigh - _rangeLow) / 1023.0) + _rangeLow;
+	_value = ((float) (_raw - _rawLow)) * ((_rangeHigh - _rangeLow) / ((float) (_rawHigh - _rawLow))) + _rangeLow;
 
 	if (_firstCycle) {
 		_avg = _value;
@@ -54,7 +57,7 @@ void AI::loop(bool t100ms, bool b1s) {
 	_isLo = _enableLo && (_isLo || _value < _lo) && _value <= _lo + delta;
 	_isHi = _enableLolo && (_isHi || _value > _hi) && _value >= _hi - delta;
 	_isHihi = _enableLolo && (_isHihi || _value > _hihi) && _value >= _hihi - delta;
-	_isBTA = _enableBTA && (_isBTA || _value == _rangeLow || _value == _rangeHigh) && (_value <= _rangeLow + delta || _value >= _rangeHigh - delta);
+	_isBTA = _enableBTA && (_isBTA || _value <= _rangeLow || _value >= _rangeHigh) && (_value <= _rangeLow + delta || _value >= _rangeHigh - delta);
 
 }
 
@@ -71,42 +74,3 @@ bool AI::lo() { return _isLo; }
 bool AI::hi() { return _isHi; }
 bool AI::hihi() { return _isHihi; }
 bool AI::bta() { return _isBTA; }
-
-/*
-class AI {
-public:
-AI(int pin, float rangeLow, float rangeHigh, float lolo, float lo, float hi, float hihi, bool enableBTA);
-
-bool lolo();
-bool lo();
-bool hi();
-bool hihi();
-bool bta();
-
-float average();
-float min();
-float max();
-
-void loop(bool t100ms);
-private:
-int _pin;
-int _raw;
-
-float _rangeLow;
-float _rangeHigh;
-float _lolo;
-float _lo;
-float _hi;
-float _hihi;
-
-float _avg;
-float _min;
-float _max;
-
-bool _enableLolo;
-bool _enableLo;
-bool _enableHi;
-bool _enableHihi;
-bool _enableBTA;
-};
-*/
