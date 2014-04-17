7d65fc71fe62586006edadd14253427dc0a2b33a
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2014-04-17 22:05:24 +0200

    Testcode

--- Arduino/ArduinoPLC/ArduinoPLC.ino
+++ Arduino/ArduinoPLC/ArduinoPLC.ino
@@ -9,19 +9,21 @@
 
 #define SENSOR 0
 #define PROBE 0
-#define LED 1
+#define LED 0
 
 General general;
+IO io;
+
 DI DIs[] = {};
-DO DOs[] = {};
+DO DOs[] = {DO(100),DO(101),DO(102)};
 AI AIs[] = {};
 AO AOs[] = {};
-IO _IO = IO();
 
 void setup() {
     Serial.begin(9600);
     
-    _IO.setRegisterOut(2,3,4,1);
+    io.setRegisterOut(8,2,4,1);
+    pinMode(7,OUTPUT);
 }
 
 int n = 0;
@@ -34,16 +36,14 @@ void loop() {
     //(voltage - 0.5) * 100.0
     float pot = ((((float)analogRead(0)) * (5.0 / 1023.0)) - 0.5) * 100.0;
     
-    Serial.println(pot);
-    
-    _IO.writeBit(100,pot > 10.0);
+    /*general.io.writeBit(100,pot > 10.0);
     _IO.writeBit(101,pot > 12.0);
     _IO.writeBit(102,pot > 14.0);
     _IO.writeBit(103,pot > 16.0);
     _IO.writeBit(104,pot > 18.0);
     _IO.writeBit(105,pot > 20.0);
-    _IO.writeBit(106,pot > 22.0);
-    _IO.writeBit(107,pot > 24.0);
+    _IO.writeBit(106,pot > 22.0);*/
+    io.writeBit(107,true);
     
     loopTypicals();
 }
@@ -58,16 +58,16 @@ void loopTypicals() {
             DIs[i].loop(general);
         }
     }
-    if(sizeof(DOs) > 0) {
-        maxObjects = sizeof(DOs) / sizeof(DOs[0]);
-        for(int i = 0; i < maxObjects; i++) {
-            DOs[i].loop(general);
-        }
-    }
     if(sizeof(AIs) > 0) {
         maxObjects = sizeof(AIs) / sizeof(AIs[0]);
         for(int i = 0; i < maxObjects; i++) {
             AIs[i].loop(general);
+        }
+    }
+    if(sizeof(DOs) > 0) {
+        maxObjects = sizeof(DOs) / sizeof(DOs[0]);
+        for(int i = 0; i < maxObjects; i++) {
+            DOs[i].loop(general,io);
         }
     }
     if(sizeof(AOs) > 0) {
@@ -77,5 +77,5 @@ void loopTypicals() {
         }
     }
     
-    _IO.write();
+    io.write();
 }
