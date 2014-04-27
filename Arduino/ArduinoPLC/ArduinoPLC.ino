665d320c6a8b6b0719137b528ca6621f1a1f5f52
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2014-04-27 20:44:54 +0200

    Test the new PID controller

--- Arduino/ArduinoPLC/ArduinoPLC.ino
+++ Arduino/ArduinoPLC/ArduinoPLC.ino
@@ -12,6 +12,7 @@
 #include <General.h>
 
 #define LED 0
+#define devLED 0
 #define POT 1
 #define LUX 0
 #define BUTTON 0
@@ -20,14 +21,15 @@
 General general;
 
 TFT tft = TFT(10,9,8);
+int line = 0;
 
 IO io;
 
 //  //
-DI DIs[] = {DI(2,true)};
+DI DIs[] = {};
 
 //  //
-DO DOs[] = {};
+DO DOs[] = {DO(2)};
 
 // LUX,POT //
 //float rangeLow, float rangeHigh, float lolo, float lo, float hi, float hihi, bool enableBTA, int rawLow, int rawHigh
@@ -39,8 +41,8 @@ AI AIs[] = {
 // int pin, float min, float max, float rate //
 AO AOs[] = {AO(3,0.0,100.0,-1)};
 
-// LUXcontrol //
-PID PIDs[] = {PID(20.0,100.0,LUX,LED)};
+// LUXcontrol // 
+PID PIDs[] = {PID(0.0,100.0,LUX,LED,0.3,0.3,0.0,5.0)};
 
 void setup() {
     Serial.begin(9600);
@@ -58,6 +60,7 @@ void loop() {
     PIDs[LUXcontrol].sp(AIs[POT].value());
     PIDs[LUXcontrol].activate(true); //DIs[BUTTON].isActive());
 
+    DOs[devLED].activate(PIDs[LUXcontrol].isDeviated());
 
 
 //  end code //
@@ -67,13 +70,13 @@ void loop() {
 void loopTypicals() {
     //_IO.read(general);
     
-    int line = 0;
-    String s;
-    char str[8];
+    if(general.t100ms) {
+        //;
     
-    if(general.t1s)
-        tft.background(0, 0, 0);
-    
+        line++;
+        if(line > 160)
+            line = 0;
+    }
     int maxObjects = 0;
     
     // INPUTS //
@@ -81,17 +84,6 @@ void loopTypicals() {
         maxObjects = sizeof(DIs) / sizeof(DIs[0]);
         for(int i = 0; i < maxObjects; i++) {
             DIs[i].loop(general);
-            
-            if(general.t1s) {
-                tft.text("DI",0,line * 8);
-                
-                if(DIs[i].isActive())
-                    tft.text("active",20,line * 8);
-                else
-                    tft.text("inactive",20,line * 8);
-                
-                line++;
-            }
         }
     }
     if(sizeof(AIs) > 0) {
@@ -99,6 +91,10 @@ void loopTypicals() {
         for(int i = 0; i < maxObjects; i++) {
             AIs[i].loop(general);
             
+            if(general.t100ms)
+                draw(AIs[i].value() / 2.0,0,(i == 0) ? 0 : 255,255);
+            
+            /*
             if(general.t1s) {
                 tft.text("AI",0,line * 8);
                 
@@ -115,7 +111,9 @@ void loopTypicals() {
                 tft.text("V",130,line*8);
                 
                 line++;
-            }
+            }*/
+            
+            
         }
     }
     
@@ -128,7 +126,9 @@ void loopTypicals() {
             AOs[PIDs[i].AO()].output(PIDs[i].output());
             AOs[PIDs[i].AO()].activate(PIDs[i].isActive());
             
-            if(general.t1s) {
+            if(general.t100ms)
+                draw(PIDs[i].output() - 1.0,255,255,255);
+            /*
                 tft.text("PID",0,line * 8);
                 
                 s = String(PIDs[i].value());
@@ -153,7 +153,7 @@ void loopTypicals() {
                 tft.text("%",70,line*8);
                 
                 line++;
-            }
+            }*/
         }
     }
     
@@ -163,7 +163,7 @@ void loopTypicals() {
         for(int i = 0; i < maxObjects; i++) {
             DOs[i].loop(general,io);
             
-            if(general.t1s) {
+            /*if(general.t1s) {
                 tft.text("DO",0,line * 8);
                 
                 if(DOs[i].isActive())
@@ -172,7 +172,7 @@ void loopTypicals() {
                     tft.text("inactive",20,line * 8);
                 
                 line++;
-            }
+            }*/
         }
     }
     if(sizeof(AOs) > 0) {
@@ -180,6 +180,9 @@ void loopTypicals() {
         for(int i = 0; i < maxObjects; i++) {
             AOs[i].loop(general);
             
+            if(general.t100ms)
+                draw(AOs[i].output(),255,0,0);
+            /*
             if(general.t1s) {
                 tft.text("AO",0,line * 8);
                 
@@ -196,9 +199,27 @@ void loopTypicals() {
                 tft.text("V",130,line*8);
                 
                 line++;
-            }
+            }*/
         }
     }
     
     io.write();
+}
+
+
+void draw(float y, int red, int green, int blue) {
+    
+    tft.stroke(30,30,30);
+    tft.point(line,20);
+    tft.point(line,120);
+    
+    tft.stroke(blue,green,red);
+    tft.point(line,(int(y) * -1) + 120);
+    
+    
+    tft.stroke(0,0,0);
+    tft.line((line + 1) % 160,0,(line + 1) % 160,128);
+    tft.line((line + 2) % 160,0,(line + 2) % 160,128);
+    tft.line((line + 3) % 160,0,(line + 3) % 160,128);
+    
 }
