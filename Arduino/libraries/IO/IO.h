ec3863662336d59510b98e28a07ade3abb2281ef
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2014-06-15 19:32:54 +0200

    Incorrect documentation

--- Arduino/libraries/IO/IO.h
+++ Arduino/libraries/IO/IO.h
@@ -5,7 +5,7 @@ IO abstraction layer.
 
 Currently supports
 - Arduino UNO
-- Output Shift Registers (Clock and Latch have to be on 0-7 and Data has to be on 8 - 13)
+- Output Shift Registers (have to be on 0-7)
 - Digital output
 
 */
